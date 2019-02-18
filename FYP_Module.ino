248c4feeb11dc091ede5d90d8bc1872a2e0a4da2
Kenneth Lee <kennethlee.id@gmail.com>
2019-02-18 16:46:33 +0800

    added LED blink function

--- FYP_Module.ino
+++ FYP_Module.ino
@@ -7,7 +7,14 @@
 
 bool deviceConnected = false;
 uint8_t link_loss_alert_level = 0;
-uint8_t NO_ALERT =0;  
+uint8_t NO_ALERT =0;
+
+void blink(uint8_t pin, uint_fast32_t interval){
+    digitalWrite(pin,HIGH);
+    delay(interval);
+    digitalWrite(pin,LOW);
+    delay(interval);
+}  
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting FYP Module!");
@@ -20,9 +27,6 @@ void loop() {
     digitalWrite(2,LOW);
   }
   if((link_loss_alert_level==2) && !deviceConnected){
-    digitalWrite(2,HIGH);
-    delay(500);
-    digitalWrite(2,LOW);
-    delay(500);
+    blink(2,500); //blink onboard LED
   }
 }
