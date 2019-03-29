819b9b972cb61eb5d8f71de4f23352f54da3e154
hansryant <hans_ryan2404@hotmail.com>
2019-03-30 00:11:47 +0800

    Modified FYP_Module.ino

--- FYP_Module.ino
+++ FYP_Module.ino
@@ -1,6 +1,8 @@
 #include "src/BLE/BLEModule.h"
 #include "src/HRModule/HRModule.h"
 #include "src/LED/LED.h"
+#include "src/BIT_HW/BIT_HW.h"
+#include "src/ECG/ECGModule.h"
 
 //TODO: create event handler for server
 //TODO: create event handler for characteristics 
@@ -10,7 +12,7 @@
 bool deviceConnected = false;
 uint8_t link_loss_alert_level = 0;
 uint8_t NO_ALERT =0;
-
+int sensor_mode = 0;
 // uint8_t currentHeartrate[2];
 
 BLEService *thermometer_service=NULL;
@@ -21,33 +23,33 @@ BLECharacteristic * heartrate_heartrate_measurement=NULL;
 BLECharacteristic * thermometer_temperature_measurement=NULL;
 
 HRModule * myHRModule = NULL;
-
+ECGModule * myECGModule = NULL;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting FYP Module!");
   initLED();
   initRGB();
-  setRGB(HIGH, HIGH, LOW, LOW);
+  initBIT_HW();
+  boot_up();
+  readBIT_HW();
+  detect_sensor_module();
+  delay(1000);
+  //setRGB(HIGH, LOW, LOW, LOW);
   // BLESetUp();
   // myHRModule = new HRModule();
+  myECGModule = new ECGModule();
 }
 
 void loop() {
   // if(deviceConnected){
-    setLED(HIGH, LOW, LOW);
-    delay(500);
-    setLED(HIGH, HIGH, LOW);
-    delay(500);
-    setLED(HIGH, HIGH, HIGH);
-    delay(500);
-    setLED(LOW, LOW, LOW);
-    delay(500);
     // currentHeartrate[0] = 0;
     // currentHeartrate[1] = 65;
     // heartrate_heartrate_measurement->setValue(currentHeartrate,2);
     // heartrate_heartrate_measurement->notify();
-
+    
+    myECGModule->startReading();
+    Serial.println(myECGModule->getECGRaw());
     // myHRModule->startReading();
     // Serial.println(myHRModule->getCurrentHeartRate());
 // 
@@ -56,4 +58,24 @@ void loop() {
   //   blink(2,500); //blink onboard LED
   // }
   // delay(4);
+}
+void boot_up() {
+  setLED(HIGH, LOW, LOW);
+  delay(300);
+  setLED(HIGH, HIGH, LOW);
+  delay(300);
+  setLED(HIGH, HIGH, HIGH);
+  delay(300);
+  setLED(LOW, LOW, LOW);
+  delay(300);
+  setRGB(HIGH, LOW, HIGH, HIGH);  
+  delay(300);
+  setRGB(HIGH, HIGH, LOW, HIGH);  
+  delay(300);
+  setRGB(HIGH, HIGH, HIGH, LOW);  
+  delay(300);
+  setRGB(HIGH, LOW, LOW, LOW);  
+  delay(300);
+  setRGB(LOW, LOW, LOW, LOW);  
+  delay(300);
 }
