07152b2abbdbc0939942f9609fb10c54961bbccf
Kenneth Lee <kennethlee.id@gmail.com>
2019-05-04 21:55:16 +0800

    Add LED class

---
+++ src/LED/LED.cpp
@@ -0,0 +1,39 @@
+#include "LED.h"
+
+LED::LED(int pin, long on, long off,bool blink){
+    ledPin = pin;
+	pinMode(ledPin, OUTPUT);     
+	this->blink= blink;
+	onTime = on;
+	offTime = off;
+	ledState = LOW; 
+	previousMillis = 0;
+}
+
+void LED::setBlink(bool blinkState){
+    this->blink= blinkState;
+}
+
+void LED::setLEDState(int state){
+    this->ledState= state;
+}
+
+void LED::updateLED(){
+    // check to see if it's time to change the state of the LED
+    if(blink==true){
+        unsigned long currentMillis = millis();
+        if((ledState == HIGH) && (currentMillis - previousMillis >= onTime)){
+            ledState = LOW;  // Turn it off
+            previousMillis = currentMillis;  // Remember the time
+            digitalWrite(ledPin, ledState);  // Update the actual LED
+        }
+        else if ((ledState == LOW) && (currentMillis - previousMillis >= offTime)){
+            ledState = HIGH;  // turn it on
+            previousMillis = currentMillis;   // Remember the time
+            digitalWrite(ledPin, ledState);	  // Update the actual LED
+        }
+    }
+    else{
+        digitalWrite(ledPin, ledState);
+    }
+}
---
+++ src/LED/LED.h
@@ -0,0 +1,20 @@
+#ifndef LED_H_
+#define LED_H_
+#include "Arduino.h"
+class LED{
+    private:
+    int ledPin;      // the number of the LED pin
+	long onTime;     // milliseconds of on-time
+	long offTime;    // milliseconds of off-time
+    bool blink;
+
+	int ledState;             		// ledState used to set the LED when blinking
+	unsigned long previousMillis;  	// will store last time LED was updated
+
+    public:
+    LED(int pin, long on, long off, bool blink);
+    void setBlink(bool blinkState);
+    void setLEDState(int state);
+    void updateLED();
+};
+#endif
